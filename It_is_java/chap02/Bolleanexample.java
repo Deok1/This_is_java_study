7d0f9eb156649b047d8714df37530e634fb572ba
Deok-il <srlaejrdlfs@gmail.com>
2025-01-03 14:40:00 +0900

    [upload] Boolean 타입

---
+++ It_is_java/chap02/Bolleanexample.java
@@ -0,0 +1,12 @@
+package chap02;
+
+public class Bolleanexample {
+	public static void main(String[] args) {
+		boolean stop = true;
+		if (stop) {
+			System.out.println("중지하겠습니다");
+		} else {
+			System.out.println("시작하겠습니다");
+		}
+	}
+}
