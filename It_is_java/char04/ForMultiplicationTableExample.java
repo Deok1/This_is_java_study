92ab68896dc3dacf44850bc4ca7bddea40312ce7
Deok-il <srlaejrdlfs@gmail.com>
2025-01-14 15:41:13 +0900

    [upload] 구구단 출력하기

---
+++ It_is_java/char04/ForMultiplicationTableExample.java
@@ -0,0 +1,13 @@
+package char04;
+
+public class ForMultiplicationTableExample {
+	public static void main(String[] args) {
+		for (int m=1; m<=9; m++) {
+			System.out.println("*** " + m + "단 ***");
+			
+			for(int n=1; n<=9; n++) {
+				System.out.println(m + " x " + " = " + (m*n) );
+			}
+		}
+	}
+}
