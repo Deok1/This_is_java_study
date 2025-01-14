820477876e162765c7f3193c46f4aa8065eea578
Deok-il <srlaejrdlfs@gmail.com>
2025-01-14 15:25:46 +0900

    [reupload] 설명 추가

--- It_is_java/char04/ForSumFrom1To100Example.java
+++ It_is_java/char04/ForSumFrom1To100Example.java
@@ -2,8 +2,8 @@ package char04;
 
 public class ForSumFrom1To100Example {
 	public static void main(String[] args) {
-		int sum = 0;
-		
+		int sum = 0;							//여기다 선언한 이유는 for문이 끝나고 나서 sum을 사용하기 때문
+												//함수 안에 있으면 함수 내에서만 사용 가능하기 때문
 		for(int i=0; i<=100; i++) {
 			sum += i;
 		}
