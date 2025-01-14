359a76583765c77df92add3c0a3e5fe5cf7bea47
Deok-il <srlaejrdlfs@gmail.com>
2025-01-14 15:32:05 +0900

    [reupload] 변수 위치 변경

--- It_is_java/char04/ForSumFrom1To100Example.java
+++ It_is_java/char04/ForSumFrom1To100Example.java
@@ -2,12 +2,14 @@ package char04;
 
 public class ForSumFrom1To100Example {
 	public static void main(String[] args) {
-		int sum = 0;							//여기다 선언한 이유는 for문이 끝나고 나서 sum을 사용하기 때문
-												//함수 안에 있으면 함수 내에서만 사용 가능하기 때문
-		for(int i=0; i<=100; i++) {
+		int sum = 0;							
+			
+		int i=0;
+		
+		for(i=1; i<=100; i++) {
 			sum += i;
 		}
 		
-		System.out.println("1~100의 합 : " + sum);
+		System.out.println("1~" + (i-1) + "의 합 : " + sum);
 	}
 }
