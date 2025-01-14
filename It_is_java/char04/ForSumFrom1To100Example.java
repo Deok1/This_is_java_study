5cbc4994707b1e6c94b007dc2a6c4d256d999935
Deok-il <srlaejrdlfs@gmail.com>
2025-01-14 15:24:34 +0900

    [upload] 1부터 100까지 합을 출력

---
+++ It_is_java/char04/ForSumFrom1To100Example.java
@@ -0,0 +1,13 @@
+package char04;
+
+public class ForSumFrom1To100Example {
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for(int i=0; i<=100; i++) {
+			sum += i;
+		}
+		
+		System.out.println("1~100의 합 : " + sum);
+	}
+}
