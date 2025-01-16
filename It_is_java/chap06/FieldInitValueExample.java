e9e29bb395917353a0df22700fba00ef83dc4aa1
Deok-il <srlaejrdlfs@gmail.com>
2025-01-16 17:49:16 +0900

    [upload] 클래스

---
+++ It_is_java/chap06/FieldInitValueExample.java
@@ -0,0 +1,18 @@
+package chap06;
+
+public class FieldInitValueExample {
+	public static void main(String[] args0 ) {
+		FieldInitValue fiv = new FieldInitValue();
+		
+		System.out.println("byteFidel : " + fiv.byteField);
+		System.out.println("shortFidel : " + fiv.shortField);
+		System.out.println("intFidel : " + fiv.intField);
+		System.out.println("longFidel : " + fiv.longField);
+		System.out.println("booleanFidel : " + fiv.booleanField);
+		System.out.println("charFidel : " + fiv.charField);
+		System.out.println("floatFidel : " + fiv.floatField);
+		System.out.println("doubleFidel : " + fiv.doubleField);
+		System.out.println("arrFidel : " + fiv.arrField);
+		System.out.println("referenceFidel : " + fiv.referenceField);
+	}
+}
